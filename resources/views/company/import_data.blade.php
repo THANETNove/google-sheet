b52448646b7cded02515eb8b363b5239533d233f
THANETNove <novel2020manga@gmail.com>
2024-10-01 16:59:14 +0700

    add

--- resources/views/company/import_data.blade.php
+++ resources/views/company/import_data.blade.php
@@ -1,25 +1,42 @@
 @extends('layouts.appHome')
 
 @section('content')
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <div class="row">
+            <div class="col-lg-12 mb-4 order-0">
+
+                <div class="card">
+                    @php
+                        // ตรวจสอบว่าตัวแปร $query ถูกตั้งค่าแล้วหรือไม่
+                        $query = isset($query) ? $query : [];
+                    @endphp
+
+                    <h5 class="card-header">รายชื่อ บริษัท</h5>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
     <script>
+        // นำค่าตัวแปร $query จาก PHP มาใช้ใน JavaScript
+        const queryData = @json($query);
+
         const action = 'getUsers';
-        const sheet = 'General Ledger Sub';
-        const id_sheet = '1NwqH-EqZsGTGCmB79m491aNd8l_Ib7kQHAUDFbhNrk8';
+        const sheets = ['General Ledger', 'General Ledger Sub', 'Account_Code'];
+        const id_sheet = queryData.id_sheet;
 
-        // URL ที่จะใช้ดึงข้อมูล
-        const url =
-            `https://script.google.com/macros/s/AKfycbyrlSPW6xRtrPCe1rHpgSUMLCL0XAXETfoPVLK5OuvYGo8SeyDV2YukV6PCcQ35tPm5/exec?action=${action}&sheet=${sheet}&id_sheet=${id_sheet}`;
+        console.log('PHP query:', queryData.id_apps_script); // แสดงข้อมูลจาก $query
 
-        // ดึงข้อมูลโดยใช้ fetch
-        /*  fetch(url)
-             .then(response => response.json()) // แปลง response เป็น JSON
-             .then(data => console.log(data)) // แสดงข้อมูลที่ได้
-             .catch(error => console.error('Error:', error)); */
-
-
-        // ดึงข้อมูลโดยใช้ axios
-        axios.get(url)
-            .then(response => console.log(response.data)) // แสดงข้อมูลที่ได้
-            .catch(error => console.error('Error:', error)); // จัดการกรณี error
+        sheets.forEach(sheet => {
+            const url =
+                `https://script.google.com/macros/s/${queryData.id_apps_script}/exec?action=${action}&sheet=${sheet}&id_sheet=${id_sheet}`;
+            axios.get(url)
+                .then(response => {
+                    console.log(`${sheet}:`, response.data);
+                    // สามารถใช้ queryData ในการประมวลผลข้อมูลเพิ่มเติมได้ที่นี่
+                })
+                .catch(error => console.error(`Error in ${sheet}:`, error));
+        });
     </script>
 @endsection
