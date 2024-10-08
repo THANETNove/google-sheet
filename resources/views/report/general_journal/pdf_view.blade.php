8567f3f111dcbd19506d04a60b731068a6985754
THANETNove <novel2020manga@gmail.com>
2024-10-09 01:00:17 +0700

    add

--- resources/views/report/general_journal/pdf_view.blade.php
+++ resources/views/report/general_journal/pdf_view.blade.php
@@ -3,24 +3,35 @@
 
 <head>
 
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>สมุดรายวันทั่วไป</title>
-    <meta charset="utf-8">
     <meta http-equiv="Content-Language" content="th" />
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <!-- thai font -->
+
+
+
+
+    <!-- ฟอนต์ภาษาไทย -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Sarabun:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Sarabun:wght@400;700&display=swap" rel="stylesheet">
+
     <style>
+        @font-face {
+            font-family: 'THSarabunNew';
+            src: url("{{ asset('fonts/THSarabunNew.ttf') }}") format('truetype');
+            font-style: normal;
+            font-weight: normal;
+        }
+
         body {
             font-size: 8pt;
             margin: 0;
-            font-family: 'Sarabun', sans-serif;
-            /* ใช้ฟอนต์ Sarabun เป็นหลัก */
+            /*   font-family: 'THSarabunNew' !important; */
+            font-family: 'Sarabun', 'Noto Sans Thai', sans-serif;
+            /* ฟอนต์ภาษาไทย */
         }
 
 
@@ -148,7 +159,7 @@
 </head>
 
 <body>
-    <div class="container-xxl flex-grow-1 container-p-y">
+    <div class="container-xxl flex-grow-1 container-p-y ">
         <div class="row">
             <div class="col-lg-12 mb-4 order-0">
 
